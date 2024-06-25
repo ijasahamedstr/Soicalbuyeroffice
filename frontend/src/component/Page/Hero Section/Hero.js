46872373344e72a8330345502ec861f3dc2b2060
ijasahamedstr <ijasahamed.str@gmail.com>
2024-06-25 17:19:29 +0300

    commit

--- frontend/src/component/Page/Hero Section/Hero.js
+++ frontend/src/component/Page/Hero Section/Hero.js
@@ -1,72 +1,90 @@
 import React from 'react';
 import './Hero.css';
-import Button from 'react-bootstrap/Button';
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../Hero Section/assets/image1-1.png";
+import TrackVisibility from 'react-on-screen';
 
 
 function HeroImage() {
+  const mystyle2 = {
+    fontFamily: "Noto Kufi Arabic", 
+    fontSize: 17,
+    fontWeight: '400',
+    position: 'relative',
+    textShadow: "0px  0px  10px  Red",
+  };
+
   const mystyle = {
     fontFamily: "Noto Kufi Arabic",
     fontWeight: '400',
-    textShadow: "0px  0px  10px  Red" 
+    textShadow: "0px  0px  10px  Red", 
   };
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "لبيع وشراء المنتجات الرقمية", "لبيع وشراء المنتجات الرقمية", "لبيع وشراء المنتجات الرقمية" ];
+  const period = 2000;
 
-  const mystyle1 = {
-    fontFamily: "Noto Kufi Arabic", 
-    color: '#6164ff',
-    fontWeight: '500',
-    textShadow: "0px  0px  10px  black"  
-  };
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
 
-  const mystyle2 = {
-    fontFamily: "Noto Kufi Arabic", 
-    fontSize: 16,
-    fontWeight: '400',
-    position: 'relative',
-    textShadow: "0px  0px  10px  Red"
-  };
+    return () => { clearInterval(ticker) };
+  }, [text])
 
-  const btn = {
-    backgroundColor:'#6164ff',
-    color :'#fff',
-    marginTop: '20px',
-    marginRight: '20px',
-    minWidth: '140px',
-    height: '45PX'
-  };
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
-  const btn1 = {
-    color :'#fff',
-    marginTop: '20px',
-    marginRight: '20px',
-    minWidth: '140px',
-    height: '45PX'
-  };
+    setText(updatedText);
 
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
  return (
-   <div className="p-7 text-center w-screen h-screen text-white" style={{
-    backgroundImage: "url('https://usr.dokan-cdn.com/img/home/home.png')", height: 500, 
-    backgroundColor: "#3D3D3D",marginTop: -24, paddingTop: '45px',fontFamily:'Noto Kufi Arabic'}}>
-     <div class="container mx-auto flex py-24 items-center justify-center flex-col">
-       <div class="text-center lg:w-5/12 w-full">
-         <h1 style={mystyle}  className="my-4 text-5xl font-bold leading-tight">
-         أفضل منصة
-         </h1>
-         <h1 style={mystyle1}  className="my-4 text-5xl font-bold leading-tight">
-         لبيع وشراء المنتجات الرقمية
-         </h1>
-         <p style={mystyle2}  className="text-2xl mb-8">
-         من خلال منصة يوزر يمكنك بيع وشراء المنتجات الرقمية بأمان وسلاسة
-         </p>
-       </div>
-     </div>
-     <div  className='btn'>
-        <Button style={btn} variant="outline-primary">شراء حساب تواصل اجتماعي</Button>{' '}
-        <Button style={btn} variant="outline-secondary">شراء حساب لعبة</Button>{' '}
-        <Button  style={btn}  variant="outline-success">شراء خدمة</Button>{' '}
-        <Button style={btn1} variant="outline-warning">بيع خلال المنصة</Button>{' '}
-     </div>
-       
-   </div >
+  <section className="banner" id="home" style={{backgroundColor:'#121212',marginTop:'-8px'}}>
+  <Container className='Containerhero'>
+    <Row className="aligh-items-center">
+      <Col xs={12} md={6} xl={7}>
+              <TrackVisibility>
+          {({ isVisible }) =>
+            <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+              <img src={headerImg} alt="Header Img"/>
+            </div>}
+        </TrackVisibility>
+      </Col>
+      <Col xs={12} md={6} xl={5}>
+      <TrackVisibility className='track' style={{paddingTop: '30px',}}>
+          {({ isVisible }) =>
+          <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+            <h2 style={{mystyle,color:'#FFFFFF',fontSize:'40px'}}>أفضل منصة</h2><br/>
+            <h1><span className="txt-rotate" dataPeriod="1000" data-rotate='[ "لبيع وشراء المنتجات الرقمية", "لبيع وشراء المنتجات الرقمية", "لبيع وشراء المنتجات الرقمية" ]'><span className="wrap">{text}</span></span></h1><br/>
+              <p style={mystyle2}>من خلال منصة يوزر يمكنك بيع وشراء المنتجات الرقمية بأمان وسلاسة</p>
+          </div>}
+        </TrackVisibility>
+      </Col>
+    </Row>
+  </Container>
+</section>
  );
 }
 
