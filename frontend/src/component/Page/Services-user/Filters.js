8ccab367b611ebcd6dfd4dc32b44b80ac8ddcf07
ijasahamedstr <ijasahamed.str@gmail.com>
2024-06-29 17:23:24 +0300

    commit

--- frontend/src/component/Page/Services-user/Filters.js
+++ frontend/src/component/Page/Services-user/Filters.js
@@ -1,9 +1,6 @@
 // Filters.js
 import React from 'react';
 import { Form } from 'react-bootstrap';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 
 const Filters = ({ filters, onFilterChange }) => {
   const handleFilterChange = (event) => {
@@ -14,23 +11,12 @@ const Filters = ({ filters, onFilterChange }) => {
   const marginTopValue = '10px',marginBottomValue = '20px';
   return (
     <>
-     <Navbar collapseOnSelect expand="lg" >
-      <Container>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-          <Form style={{width:'300px'}}>
-        <h4 style={{marginTop:marginTopValue,marginBottom:marginBottomValue,color:'rgb(97, 100, 255)'}}>البحث المخصص</h4>
-        <Form.Group className="mb-3">
-          <Form.Label>نوع الحساب:</Form.Label>
-          <Form.Control  type="text" name="location" value={filters.location} onChange={handleFilterChange} />
+     <Form className='centered-form'>
+        <h4 style={{marginTop:marginTopValue,marginBottom:marginBottomValue,color:'rgb(97, 100, 255)',textAlign:'center'}}>البحث المخصص</h4>
+        <Form.Group style={{justifyContent:'center'}}>
+          <Form.Control style={{marginTop:marginTopValue,marginBottom:marginBottomValue,textAlign:'right',justifyContent:'center'}}  type="text" name="location" value={filters.location} onChange={handleFilterChange} />
         </Form.Group>
       </Form>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    
     </>
   );
 };
